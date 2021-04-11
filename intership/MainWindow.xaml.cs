3746cab26c538ea8906f954d2767fd47ce7a57a9
heisen1berg <rybtsov_i_v@mail.ru>
2021-04-11 16:02:45 +0300

    Добавьте файлы проекта.

---
+++ intership/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using intership.dict;
+
+namespace intership
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dict dictSearch = new Dict("word.txt");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    private void search(object sender, RoutedEventArgs e)
+        {
+            string text = In.Text;
+            string[] ans = dictSearch.search(text);
+            Out.Text = "";
+
+            string Text = "";
+            
+            for (int i = 0; i < ans.Length; i++)
+            {
+                Text += ans[i];
+                Text += "\r\n";
+            }
+            Out.Text += Text;
+        }
+    }
+}
---
+++ intership/dict/Dict.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace intership.dict
+{
+    class Dict
+    {
+        public string path;
+        public Dict(string path)
+        {
+            this.path = path;
+        }
+
+        public string[] search(string Word)
+        {
+            using TextReader file = File.OpenText("../../../words.txt");
+
+            List<string> results = new List<string>();
+            string line = file.ReadLine();
+            while(line != null)
+            {
+                if (line.ToLower().Contains(Word.ToLower()))
+                {
+                    results.Add(line);
+                }
+
+                line = file.ReadLine();
+            }
+
+            return results.ToArray();
+        }
+    }
+}
